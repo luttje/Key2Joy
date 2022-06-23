a9be6d9ff7b524c7391c8d4ba8a17b6dd5f53782
Luttje <2738114+luttje@users.noreply.github.com>
2022-06-23 22:12:53 +0200

    first Lua test

---
+++ KeyToJoy/Mapping/Actions/Logic/LuaScriptAction.cs
@@ -0,0 +1,86 @@
+﻿using Neo.IronLua;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyToJoy.Mapping
+{
+    [Action(
+        Description = "Lua Script Action",
+        OptionsUserControl = typeof(LuaScriptActionControl),
+        NameFormat = "Lua Script: {0}"
+    )]
+    internal class LuaScriptAction : BaseAction
+    {
+        [JsonProperty]
+        public string Script { get; set; }
+
+        private Lua lua;
+
+        public LuaScriptAction(string name, string description)
+            : base(name, description)
+        { }
+
+        internal override async Task Execute(InputBag inputBag)
+        {
+            dynamic g = lua.CreateEnvironment<LuaGlobal>();
+            // register new functions
+            g.print = new Action<object[]>(Print);
+
+            g.dochunk(Script, "KeyToJoy.Script.Inline.lua");
+        }
+
+        private void Print(object[] objects)
+        {
+            foreach (var obj in objects)
+            {
+                System.Diagnostics.Debug.Write(obj);
+            }
+            System.Diagnostics.Debug.WriteLine("");
+        }
+
+        internal override void OnStartListening()
+        {
+            base.OnStartListening();
+
+            lua = new Lua();
+        }
+
+        internal override void OnStopListening()
+        {
+            base.OnStopListening();
+
+            lua.Dispose();
+        }
+
+        public override string GetNameDisplay()
+        {
+            // Truncate the script to be no more than 50 characters
+            string truncatedScript = Script.Length > 47 ? Script.Substring(0, 47) + "..." : Script;
+
+            return Name.Replace("{0}", truncatedScript);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LuaScriptAction action))
+                return false;
+
+            return action.Name == Name
+                && action.Script == Script;
+        }
+
+        public override object Clone()
+        {
+            return new LuaScriptAction(Name, description)
+            {
+                Script = Script,
+                ImageResource = ImageResource,
+                Name = Name,
+            };
+        }
+    }
+}
---
+++ KeyToJoy/Mapping/Actions/Logic/LuaScriptActionControl.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace KeyToJoy.Mapping
+{
+    partial class LuaScriptActionControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtScript = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblInfo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblInfo.Location = new System.Drawing.Point(5, 5);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(254, 15);
+            this.lblInfo.TabIndex = 12;
+            this.lblInfo.Text = "Lua Script:";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtScript
+            // 
+            this.txtScript.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtScript.Location = new System.Drawing.Point(5, 20);
+            this.txtScript.MaximumSize = new System.Drawing.Size(256, 256);
+            this.txtScript.Multiline = true;
+            this.txtScript.Name = "txtScript";
+            this.txtScript.Size = new System.Drawing.Size(254, 132);
+            this.txtScript.TabIndex = 13;
+            this.txtScript.TextChanged += new System.EventHandler(this.txtScript_TextChanged);
+            // 
+            // LuaScriptActionControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Controls.Add(this.txtScript);
+            this.Controls.Add(this.lblInfo);
+            this.Name = "LuaScriptActionControl";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Size = new System.Drawing.Size(264, 157);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtScript;
+    }
+}
