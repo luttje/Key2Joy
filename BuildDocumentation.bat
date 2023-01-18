bcb0d5f78da122e0fc386865a28a25f24d2d690d
luttje <2738114+luttje@users.noreply.github.com>
2023-01-18 19:25:39 +0100

    switch to x86 completely + experiment with VS Setup

--- BuildDocumentation.bat
+++ BuildDocumentation.bat
@@ -2,4 +2,4 @@
 
 SET LOCAL=%~dp0
 set TARGET=bin\Release
-%LOCAL%BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%%TARGET%\Key2Joy\Key2Joy.xml %LOCAL%Docs --filter markdown-doc
+%LOCAL%BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%bin\x86\Release\Key2Joy\Key2Joy.xml %LOCAL%Docs --filter markdown-doc
