b79c7233ff78b92841b1a74e11db8fd6868f1af1
luttje <2738114+luttje@users.noreply.github.com>
2023-04-05 17:21:52 +0200

    migrate to dotnet sdk style projects

--- Key2Joy.Cmd/Properties/Resources.Designer.cs
+++ Key2Joy.Cmd/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace Key2Joy.Cmd.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Key2Joy.Cmd.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Key2Joy.Cmd.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace Key2Joy.Cmd.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Key2Joy.Cmd.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
-        internal static System.Drawing.Icon app_icons {
+        public static System.Drawing.Icon app_icons {
             get {
                 object obj = ResourceManager.GetObject("app_icons", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
