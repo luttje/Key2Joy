fccd24963b65232f1bf94c9bcd184316511c4601
luttje <2738114+luttje@users.noreply.github.com>
2023-04-05 18:20:37 +0200

    debug build script

--- BuildDocumentation.bat
+++ BuildDocumentation.bat
@@ -2,4 +2,7 @@
 
 SET LOCAL=%~dp0
 set TARGET=bin\Release
+echo %LOCAL%bin\Release\Key2Joy\Key2Joy.xml
+if exist %LOCAL%bin\Release\Key2Joy\Key2Joy.xml echo Key2Joy.xml exists
+if not exist %LOCAL%bin\Release\Key2Joy\Key2Joy.xml echo Key2Joy.xml does not exist
 %LOCAL%BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%bin\Release\Key2Joy\Key2Joy.xml %LOCAL%Docs --filter markdown-doc
