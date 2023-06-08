4a0978266a73fe31ec27e63ce1624d05a54ca365
luttje <2738114+luttje@users.noreply.github.com>
2023-06-08 22:59:05 +0200

    Change solution hierarchy

--- BuildDocumentation.bat
+++ BuildDocumentation.bat
@@ -4,4 +4,4 @@ IF "%CI%"=="true" GOTO :EOF
 
 SET LOCAL=%~dp0
 set TARGET=bin\Release
-%LOCAL%BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%bin\Release\Key2Joy\Key2Joy.xml %LOCAL%Docs --filter markdown-doc
+%LOCAL%Support\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%bin\Release\Key2Joy\Key2Joy.xml %LOCAL%Docs --filter markdown-doc
---
+++ Plugins/Key2Joy.Plugin.HelloWorld/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Key2Joy.Plugin.HelloWorld;
+public class Class1
+{
+
+}
