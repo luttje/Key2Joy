3095488843a1a3c8067942523c8c9d00e0fbf5cd
luttje <2738114+luttje@users.noreply.github.com>
2023-06-09 22:21:44 +0200

    remove not needed code in wrapped plugin type

--- Core/Key2Joy.Contracts/Plugins/WrappedPluginType.cs
+++ Core/Key2Joy.Contracts/Plugins/WrappedPluginType.cs
@@ -8,49 +8,23 @@ using Key2Joy.Contracts.Mapping;
 namespace Key2Joy.Contracts.Plugins
 {
     /// <summary>
-    /// Serves as the place where the actual type instances are stored (only on the host). A reference
-    /// number is returned for later use.
-    /// </summary>
-    public static class WrappedPluginTypesStore
-    {
-        private static Dictionary<int, object> instances = new Dictionary<int, object>();
-        private static int nextId = 0;
-
-        public static int Store(object instance)
-        {
-            instances.Add(nextId, instance);
-            return nextId++;
-        }
-
-        public static object Get(int id)
-        {
-            return instances[id];
-        }
-
-        public static T Get<T>(int id)
-        {
-            return (T)instances[id];
-        }
-    }
-
-    /// <summary>
     /// Type to be used as a parameter to a scripting action. When invoked, the
     /// host will be the one to execute the action.
     /// </summary>
     public class WrappedPluginType : MarshalByRefObject
     {
-        private int id;
+        private object instance;
 
-        public WrappedPluginType(object action)
+        public WrappedPluginType(object instance)
         {
-            id = WrappedPluginTypesStore.Store(action);
+            this.instance = instance;
         }
 
-        public T GetInstance<T>() => WrappedPluginTypesStore.Get<T>(id);
+        public T GetInstance<T>() => (T)instance;
 
         public void AsDelegateInvoke(params object[] args)
         {
-            var action = WrappedPluginTypesStore.Get<Delegate>(id);
+            var action = GetInstance<Delegate>();
             action.DynamicInvoke(new object[] { args }); // Extra wrap needed for params: http://deploytonenyures.blogspot.com/2011/11/c-params-and-methodinfoinvoke.html
         }
     }
