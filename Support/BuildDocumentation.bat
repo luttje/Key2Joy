8ce67ff140a927a681eabd16225e75a551b57de7
luttje <2738114+luttje@users.noreply.github.com>
2023-10-09 10:03:14 +0200

    Include all projects in tests, disable building docs on debug

--- Support/BuildDocumentation.bat
+++ Support/BuildDocumentation.bat
@@ -4,5 +4,10 @@ IF "%CI%"=="true" GOTO :EOF
 
 SET LOCAL=%~dp0
 set TARGET=%1
+
+IF "%TARGET%" == "Debug" goto skip
+
 echo "%LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%..\bin\Release\Key2Joy\Key2Joy.xml %LOCAL%..\Docs --filter markdown-doc"
 IF EXIST "%LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe" %LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%..\bin\Release\Key2Joy\Key2Joy.xml %LOCAL%..\Docs --filter markdown-doc
+
+:skip
