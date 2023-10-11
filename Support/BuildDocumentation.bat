569923b9720c3952b0e82545f47653f4f6e96093
luttje <2738114+luttje@users.noreply.github.com>
2023-10-11 19:33:07 +0200

    Fix build order with docs

--- Support/BuildDocumentation.bat
+++ Support/BuildDocumentation.bat
@@ -9,6 +9,6 @@ set BUILD_DIR=%2
 IF "%TARGET%" == "Debug" goto skip
 
 echo "%LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %BUILD_DIR% %LOCAL%..\bin\DocumentationFiles %LOCAL%..\Docs --filter markdown-doc"
-IF EXIST "%LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe" %BUILD_DIR% %LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %LOCAL%..\bin\DocumentationFiles %LOCAL%..\Docs --filter markdown-doc
+IF EXIST "%LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe" %LOCAL%BuildMarkdownDocs\bin\BuildMarkdownDocs\%TARGET%\BuildMarkdownDocs.exe %BUILD_DIR% %LOCAL%..\bin\DocumentationFiles %LOCAL%..\Docs --filter markdown-doc
 
 :skip
