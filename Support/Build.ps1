3c871356c2ca0e8443349c649190bab41d169b2e
luttje <2738114+luttje@users.noreply.github.com>
2023-10-14 12:37:49 +0200

    add pre-release workflow

--- Support/Build.ps1
+++ Support/Build.ps1
@@ -1,7 +1,16 @@
-param($config = "Debug")
+param(
+    [string] $config = "Debug",
+    [string] $versionId = $null
+)
 
 $unixTime = ([DateTimeOffset](Get-Date)).ToUnixTimeSeconds()
 
-Write-Output "Creating new '$config' build @ $unixTime"
+if (!$versionId) {
+    $versionId = $unixTime
+}
+
+Write-Output "Creating new '$config' build @ $unixTime with version identifier: '$versionId'"
 
 dotnet msbuild /p:VersionSuffix=alpha.$unixTime /p:Configuration=$config
+
+return $versionId
