2d2a5b4ef0b386951f73e8a585057afb5103c6ac
luttje <2738114+luttje@users.noreply.github.com>
2023-10-14 16:10:24 +0200

    trim version id and make version identifier complete

--- Support/Build.ps1
+++ Support/Build.ps1
@@ -9,8 +9,16 @@ if (!$versionId) {
     $versionId = $unixTime
 }
 
+$versionId = $versionId.Substring(0, [Math]::Min(10, $versionId.Length))
+
+$versionId = "alpha.$versionId"
+
 Write-Output "Creating new '$config' build @ $unixTime with version identifier: '$versionId'"
 
-dotnet msbuild /p:VersionSuffix=alpha.$unixTime /p:Configuration=$config
+dotnet msbuild /p:VersionSuffix=$versionId /p:Configuration=$config
+
+$versionPrefix = [xml](Get-Content Core/Key2Joy.Core/Key2Joy.Core.csproj) | Select-Xml -XPath "/Project/PropertyGroup/VersionPrefix" | Select-Object -ExpandProperty Node | Select-Object -ExpandProperty "#text"
+
+$versionId = "$versionPrefix-$versionId"
 
 Write-Output $versionId
